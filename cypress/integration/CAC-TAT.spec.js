b4dd8ca7f1e97d209392be48e56587d001ce4640
tatasb-Thoughtworks <tamiris.fernandes@thoughtworks.com>
2023-03-07 19:44:57 -0300

    adicionando testes com invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -73,8 +73,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('envia o formuário com sucesso usando um comando customizado', function() {
+        cy.clock()
+
         cy.fillMandatoryFieldsAndSubmit()
+
         cy.get('.success').should('be.visible')
+        cy.tick(3000)
+        cy.get('.success').should('not.be.visible')
     })
 
     it('seleciona um produto (YouTube) por seu texto', function (){
@@ -154,4 +159,43 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('be.visible')
     });
 
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Mensagem enviada com sucesso.')
+            .invoke('hide')
+            .should('not.be.visible')
+        cy.get('.error')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Valide os campos obrigatórios!')
+            .invoke('hide')
+            .should('not.be.visible')
+    })
+
+    it('preenche a area de texto usando o comando invoke ', function () {
+        const longText = Cypress._.repeat('bla bla bla et ', 20)
+        cy.get('#open-text-area')
+            .invoke('val', longText).should('have.value', longText)
+    });
+
+    it('faz uma requisição HTTP ', function () {
+        //podemos testar API dessa maneira
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function (response){
+                const { status, statusText, body} = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.contains('CAC TAT')
+            })
+    });
+
+    it.only('encontra o gato escondido', function () {
+        cy.get('#cat').invoke('show').should('be.visible')
+        //posso mudar textos com o invoke tb
+        cy.get('#title').invoke('text', 'TATA')
+    });
 })
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,10 @@
-it('testa a página da política de privacidade de forma independente ', function () {
-    cy.visit('./src/privacy.html')
-    cy.contains('CAC TAT - Política de privacidade')
-        .should('be.visible')
-});
+Cypress._.times(5, function () {
+//esse comando me ajuda a validar, por exemplo, que um teste é estavel, pois passa as 5 vezes que executa!!
+
+    it('testa a página da política de privacidade de forma independente ', function () {
+        cy.visit('./src/privacy.html')
+        cy.contains('CAC TAT - Política de privacidade')
+            .should('be.visible')
+    });
+
+})
